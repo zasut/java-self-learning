7132f11583f19298841c3f7131c96e4e9194cab2
jaden <66454947+zasut@users.noreply.github.com>
2026-01-20 22:03:27 -0800

    More method practice

---
+++ workbook 1d/src/Menu.java
@@ -0,0 +1,11 @@
+void main() {
+    displayMenu();
+    displayMenu();
+}
+
+public static void displayMenu() {
+    System.out.println("\"=== MENU ===\"\n" +
+            "\"1. Coffee - $3.99\"\n" +
+            "\"2. Tea - $2.99\"\n" +
+            "\"3. Cookie - $1.99\"");
+}
