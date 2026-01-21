d40c523b27b6e283a835b7609905d95e9ee44d6b
jaden <66454947+zasut@users.noreply.github.com>
2026-01-20 22:01:23 -0800

    Method Practice

---
+++ workbook 1d/src/MethodPractice.java
@@ -0,0 +1,17 @@
+void main() {
+    SayHello();
+    SayGoodbye();
+    SayGoodMorning();
+}
+
+public static void SayHello() {
+    System.out.println("Hello, World!");
+}
+
+public static void SayGoodbye() {
+    System.out.println("Good bye!");
+}
+
+public static void SayGoodMorning() {
+    System.out.println("Good Morning!");
+}
